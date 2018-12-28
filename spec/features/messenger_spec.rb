81a50eb13e45c85fa465a1df10fe9f92d4e3be65
stonefarmer9 <g.p.levaillant@gmail.com>
2018-12-28 14:17:14 +0000

    Add feature test for truncated messages - test passes

--- spec/features/messenger_spec.rb
+++ spec/features/messenger_spec.rb
@@ -18,4 +18,14 @@ feature "Page displays past messages" do
     expect(page).to have_content("2")
     expect(page).to have_content("3")
   end
+
+
+  scenario 'displaying only the first 20 characters of a message' do
+    visit '/'
+    fill_in :message, with: 'This Message Exceeds 20 Characters'
+    click_button 'Submit'
+
+    expect(page).to have_link 'This Message Exceeds'
+    expect(page).not_to have_content ' 20 Characters'
+  end
 end
