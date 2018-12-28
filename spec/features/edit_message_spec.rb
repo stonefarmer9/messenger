7502a7a1c42094e6eb59fd258e20db1702eac797
stonefarmer9 <g.p.levaillant@gmail.com>
2018-12-28 14:40:55 +0000

    Add feature test for editing message, not passing currently

---
+++ spec/features/edit_message_spec.rb
@@ -0,0 +1,13 @@
+require_relative 'web_helper'
+
+feature "Editing a message" do
+  scenario "Can edit a message" do
+    add_and_view_full_message
+    click_button 'Edit'
+    fill_in :update, with: "REDACTED ON ORDER OF ORDO MALLEUS"
+    click_button 'Submit'
+
+    expect(page).to have_content("REDACTED ON ORDER OF ORDO MALLEUS")
+    expect(page).not_to have_content("Walk softly and carry a big gun")
+  end
+end
